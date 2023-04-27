a479048be6802193935a8065c770dd9610e0355b
unknown <jakab.botond@ckik.hu>
2023-04-27 20:19:10 +0200

    Kártya leosztás

--- JatekosEllen.js
+++ JatekosEllen.js
@@ -1,3 +1,4 @@
+//Globál változók
 var kartyaAdatok = [
 {id:1,value:2,sign:''},
 {id:2,value:2,sign:''},
@@ -51,20 +52,58 @@ var kartyaAdatok = [
 {id:50,value:3,sign:''},
 {id:51,value:4,sign:''},
 {id:52,value:4,sign:''}];
-
+var KartyaKirakasSzamlalo = 0;
+var KartyaIndexH = [10,11,20,21,30,31,40,41,50,51];
 var kevert = kartyaAdatok;
+var divSzam;
+var KartyaID;
+
 
 function random(felso, also){
-    return Math.floor(Math.random()*(felso-also+1)+also)
+    return Math.floor(Math.random()*(felso-also+1)+also);
 }
 
 function Kever(){
     for (let i = 0; i < (kevert.length)*2; i++) {
-        var r1 = random(0,kevert.length-1);
-        var r2 = random(0,kevert.length-1);
-        var c = kevert[r1];
+        let r1 = random(0,kevert.length-1);
+        let r2 = random(0,kevert.length-1);
+        let c = kevert[r1];
         kevert[r1] = kevert[r2];
-        
+        kevert[r2] = c;
     }
 }
 
+function general(){
+    Kever();
+    Elrejtes();
+    Kartyak();
+}
+
+function Elrejtes(){
+    let button = document.getElementById("InditoGomb");
+    button.style.visibility = "hidden";
+    button.removeAttribute("onclick","general()");
+    document.getElementById("ChipTabla").classList.remove("TablaFelnyilas");
+    document.getElementById("ChipTablaNev").classList.remove("NevFelnyilas");
+}
+
+function Kartyak(){
+    divSzam = KartyaIndexH[KartyaKirakasSzamlalo++];
+    KartyaID = kevert[kevert.length-1].id;
+    setTimeout(KartyaMegjelenites,500);
+}
+
+function KartyaMegjelenites(){
+    if(KartyaKirakasSzamlalo < 11){
+        kevert.splice(kevert.indexOf(kevert.length-1),1)
+        let img = document.createElement("img");
+        let div = document.getElementById("OLBDiv"+divSzam);
+        img.style.width = "100%";
+        img.style.height = "99.8%";
+        img.src = "kep/"+KartyaID+".png";
+        div.appendChild(img);
+        divSzam = KartyaIndexH[KartyaKirakasSzamlalo++];
+        KartyaID = kevert[kevert.length-1].id;
+        setTimeout(KartyaMegjelenites,500);
+    }
+}
